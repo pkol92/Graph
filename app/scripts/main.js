51b4bb32eb880cfd5846df1ef0ccd293987bea80
pkol92 <paulina_kolasa@wp.pl>
2022-01-30 17:33:06 +0100

    change gruop age function and name functions

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,26 +3,26 @@ const url = "https://randomuser.me/api/?gender=male&nat=fr&results=1000"
 const tableTitles = ["First Name", "Last Name", "Age", "City"];
 
 //function to create the table
-function addRow(row, item){
-    const nameCell = row.insertCell();
-    nameCell.innerText = item;
+function addCell(row, text){
+    const cell = row.insertCell();
+    cell.innerText = text;
 }
 
-function addMen(men){
+function addRow(person){
     const tbl = document.querySelector("#table");
     const row = tbl.insertRow();
-    addRow(row, men.name.first);
-    addRow(row, men.name.last);
-    addRow(row, men.dob.age);
-    addRow(row, men.location.city);
+    addCell(row, person.name.first);
+    addCell(row, person.name.last);
+    addCell(row, person.dob.age);
+    addCell(row, person.location.city);
 };
 
-function createTable(men, fieldTitles) {
+function createTable(data, titles) {
     const table = document.createElement("table");
     table.setAttribute("id", "table");
     const tableDiv = document.getElementById('tableDiv')
 
-    fieldTitles.forEach((fieldTitle) => {
+    titles.forEach((fieldTitle) => {
         let th = document.createElement('th');
         th.appendChild(document.createTextNode(fieldTitle));
         table.append(th);
@@ -31,7 +31,7 @@ function createTable(men, fieldTitles) {
     tableDiv.append(table);
 
     for (let i = 0; i < 10; i++) {
-        addMen(men[i])
+        addRow(data[i])
     };        
 }
 
@@ -41,7 +41,7 @@ function deleteTable() {
 }
 
 //create graph
-function createGraph(dataMan) {
+function createGraph(data) {
     const canvas = document.createElement("canvas");
     canvas.setAttribute("id", "chart");
     const graphDiv = document.getElementById("graphDiv");
@@ -54,7 +54,7 @@ function createGraph(dataMan) {
             labels: ['20-29','30-39', '40-49', '50-59','60-69', '70-79'],
             datasets: [{
                 label: "Number of men at a certain age",
-                data: [...dataMan],
+                data: [...data],
                 skipNull: true,
                 scales: { 
                     offset: true,
@@ -91,33 +91,34 @@ function downloadData(apiUrl) {
     })
     .then((data) => {
     hideLoading()
-    //data to graph
-    const age20 = [];
-    const age30 = [];
-    const age40 = [];
-    const age50 = [];
-    const age60 = [];
-    const age70 = [];
-    const groupsMan = [];
 
     //adding men from data to different age groups
+    let age20 = 0;
+    let age30 = 0;
+    let age40 = 0;
+    let age50 = 0;
+    let age60 = 0;
+    let age70 = 0;
+    let groupsMan = [];
+
     for (let i = 0; i<data.results.length; i++){
-        if ((data.results[i].dob.age >= 20) && (data.results[i].dob.age < 30)) {
-            age20.push(data.results[i].dob.age)
-        } else if ((data.results[i].dob.age >= 30) && (data.results[i].dob.age < 40)) {
-            age30.push(data.results[i].dob.age)
-        } else if ((data.results[i].dob.age >= 40) && (data.results[i].dob.age < 50)) {
-            age40.push(data.results[i].dob.age)
-        } else if ((data.results[i].dob.age >= 50) && (data.results[i].dob.age < 60)) {
-            age50.push(data.results[i].dob.age)
-        } else if ((data.results[i].dob.age >= 60) && (data.results[i].dob.age < 70)) {
-            age60.push(data.results[i].dob.age)
+        let age = data.results[i].dob.age
+        if ((age >= 20) && (age < 30)) {
+            age20++
+        } else if ((age >= 30) && (age < 40)) {
+            age30++
+        } else if ((age >= 40) && (age < 50)) {
+            age40++
+        } else if ((age >= 50) && (age < 60)) {
+            age50++
+        } else if ((age >= 60) && (age < 70)) {
+            age60++
         }  else {
-            age70.push(data.results[i].dob.age)
+            age70++
         }
     };
-    
-    groupsMan.push(age20.length, age30.length, age40.length, age50.length, age60.length, age70.length);
+
+    groupsMan.push(age20, age30, age40, age50, age60, age70);
 
     //sort men age to the table
     const sortAge = data.results.sort((a, b) => b.dob.age - a.dob.age);
